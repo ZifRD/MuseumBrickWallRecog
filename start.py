678a8161d2764d974e78953393755c19e6b96b8f
Ivan <62903628+ZifRD@users.noreply.github.com>
2020-08-10 16:51:12 +0300

    Delete start.py

--- start.py
+++
@@ -1,9 +0,0 @@
-from os.path import join  
-from google.colab import drive
-
-ROOT = "/content/drive"  
-drive.mount(ROOT) 
-
-PROJECT_PATH = "/content/drive/My Drive/projects/MuseumBrickWallRecog/MuseumBrickWallRecog"
-
-%cd {PROJECT_PATH}
